4d3918e2d143d03eef8aee94e2eed919d1a22d0b
Omm355 <sahooommanshuman@gmail.com>
2024-06-28 20:38:29 +0530

    Update Sign_Extend.v

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -4,7 +4,7 @@ module Sign_Extend (In,Imm_Ext,ImmSrc);
     input ImmSrc;
     output [31:0]Imm_Ext;
 
-    assign Imm_Ext = (ImmSrc == 1'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
+    assign Imm_Ext = (ImmSrc == 2'b1) ? ({{20{In[31]}},In[31:25],In[11:7]}):
                                         {{20{In[31]}},In[31:20]};
                                 
 endmodule
